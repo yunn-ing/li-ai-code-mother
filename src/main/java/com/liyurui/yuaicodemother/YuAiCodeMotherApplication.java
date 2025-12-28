f26e6495cc6a2e8e5b714cd24505f4cd7a8b42f9
云樱馨怡 <3102908245@qq.com>
2025-12-28 16:31:08 +0800

    后端初始化

---
+++ src/main/java/com/liyurui/yuaicodemother/YuAiCodeMotherApplication.java
@@ -0,0 +1,16 @@
+package com.liyurui.yuaicodemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy(exposeProxy = true)// 启用AOP代理对象暴露功能, 以支持内部方法调用的切面功能,如事务等,详见 https://docs.spring.io/spring-framework/docs/current/reference/html/core.html#aop-proxying
+
+public class YuAiCodeMotherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuAiCodeMotherApplication.class, args);
+    }
+
+}
